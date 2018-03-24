df206bdf4ec438f94f45415c91120ab11847c571
Gregory Bryk <gbryk1@gmail.com>
2018-03-25 00:27:41 +0100

    literówka w bubblesort i nowy quicksort

--- sortowania.py
+++ sortowania.py
@@ -1,5 +1,5 @@
-import time
-import random
+from time import time
+from random import random, choice
 
 
 def insertion_sort(dane):
@@ -20,48 +20,21 @@ def selection_sort(dane):
 
 
 def bubble_sort(dane):
-    sorted = 0
-    while not sorted:
-        sorted = 1
+    is_sorted = 0
+    while not is_sorted:
+        is_sorted = 1
         for i in range(len(dane) - 1):
             if dane[i] > dane[i + 1]:
                 dane[i], dane[i + 1] = dane[i + 1], dane[i]
-                sorted = 0
+                is_sorted = 0
     return dane
 
 
-# quick sort
-
-def porzadkowanie(dane, l, r):
-    indexPodzialu = wybierzpunkt(l, r)
-    wartoscPodzialu = dane[indexPodzialu]
-    print(wartoscPodzialu)
-    dane[indexPodzialu], dane[r] = dane[r], dane[indexPodzialu]
-    koniec = r
-    r -= 1
-    while r >= l:
-        if dane[r] <= wartoscPodzialu:
-            if dane[l] >= wartoscPodzialu:
-                dane[l], dane[r] = dane[r], dane[l]
-                l += 1
-                r -= 1
-                print("zmienione ", dane)
-            else:
-                l += 1
-        else:
-            r -= 1
-    dane[l], dane[koniec] = dane[koniec], dane[l]
-
-    print("posortowane", dane)
-    return l
-
-
-def quick_sort(dane, l, r):
-    if l < r:
-        x = porzadkowanie(dane, l, r)
-        quick_sort(dane, l, x - 1)
-        quick_sort(dane, x + 1, r)
-    return dane
+def quick_sort(dane):
+    if len(dane) <= 1: return dane
+    dv = choice(dane)
+    d1, d2 = [x for x in dane if x < dv], [x for x in dane if x > dv]
+    return quick_sort(d1) + [dv] * dane.count(dv) + quick_sort(d2)
 
 
 def wybierzpunkt(l, r):
@@ -155,10 +128,10 @@ def podzial(dane):
 def run():
     dane = gen_list(10, 50)
     print(dane)
-    start_time = time.time()
-    dane = bubble_sort(dane)
+    start_time = time()
+    dane = quick_sort(dane)
     print(dane)
-    return time.time() - start_time
+    return time() - start_time
 
 
 def gen_list(size, ran):
